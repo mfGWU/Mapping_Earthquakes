b42b9141a45c4de381be3369bcca0e022137038e
mfGWU <68978271+mfGWU@users.noreply.github.com>
2020-12-19 14:08:50 -0500

    Added Color and a Popup for Each Earthquake
    Added Color and a Popup for Each Earthquake (13.6.3).

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -70,9 +70,10 @@ function styleInfo(feature) {
   return {
     opacity: 1,
     fillOpacity: .5,
-    fillColor: "#ffae42",//#ffae42//#0F52BA
+    //fillColor: "#ffae42",//#ffae42//#0F52BA
+    fillColor: getColor(feature.properties.mag),
     color: "#000000",
-    radius: getRadius(),
+    radius: getRadius(feature.properties.mag),
     stroke: true,
     weight: 0.5
   };
@@ -85,6 +86,26 @@ function getRadius(magnitude) {
     return 1;
   }
   return magnitude * 4;
+}
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
 }
 // // Accessing the Toronto airline routes GeoJSON URL.
 // let torontoData = "https://raw.githubusercontent.com/mfGWU/Mapping_Earthquakes/main/torontoRoutes.json";
@@ -132,15 +153,20 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   console.log(data);
   
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data, {
+  L.geoJson(data, {
 
   // We turn each feature into a circleMarker on the map.
   
-  pointToLayer: function(feature, latlng) {
-              console.log(data);
-              return L.circleMarker(latlng);
-          },
+    pointToLayer: function(feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+      },
   // We set the style for each circleMarker using our styleInfo function.
-  style: styleInfo
-      }).addTo(map);
+    style: styleInfo,
+      // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+    }).addTo(map);
   });
