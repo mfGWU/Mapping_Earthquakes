1f045cf68cb88292604cb22b8221d83b8265c2d7
mfGWU <68978271+mfGWU@users.noreply.github.com>
2020-12-20 22:49:50 -0500

    Completed Module 13 Challenge.
    Completed Module 13 Challenge.

---
+++ Earthquake_Challenge/static/js/logicStep5.js
@@ -0,0 +1,153 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
+};
+
+//LogicStep4 addition
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+//LogicStep4 addition
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+// //LogicStep4 additionThen we add a control to the map that will allow the user to change
+// which layers are visible. (//LogicStep4 addition ++ overlays)
+L.control.layers(baseMaps, overlays).addTo(map);
+
+
+// logicStep2.js added==This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: .5,
+    //fillColor: "#ffae42",//#ffae42//#0F52BA
+    //logicStep3 Added == 
+    fillColor: getColor(feature.properties.mag),
+    color: "#000000",
+    //logicStep2 Added == 
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
+
+// logicStep2 Added == This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+// logicStep3 Added ==This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
+
+
+// // Grabbing our GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  console.log(data);
+  
+// logicStep1 Added ==.Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+
+  // We turn each feature into a circleMarker on the map.
+  
+    pointToLayer: function(feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+      },
+  // logicStep2 addedd== We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+      // logicStep3==We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+    //LogicStep4 addition == Edit your addTo(map) function at the end of your L.geoJSON() layer code
+    //to add the earthquakes overlay  (LogicStep4 addition)
+    }).addTo(earthquakes);
+
+    // LogicStep4 addition -- Then we add the earthquake layer to our map.
+    earthquakes.addTo(map);
+
+    //LogicStep5 addition == Custom Legend Control
+    var legend = L.control({
+      position: 'bottomright'
+    });
+
+    legend.onAdd = function () {
+      let div = L.DomUtil.create("div", "info legend");
+      const magnitudes = [0, 1, 2, 3, 4, 5];
+      const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+      ];
+
+    // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+      console.log(colors[i]);
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
+      return div;
+    };
+
+    legend.addTo(map);
+
+
+  });
